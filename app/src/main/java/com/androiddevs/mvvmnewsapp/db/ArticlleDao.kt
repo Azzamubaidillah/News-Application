b4b3ca190524809d93ade09b7f50679abfa8baab
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2021-05-21 01:23:17 +0700

    dao and article setup room

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/db/ArticlleDao.kt
@@ -0,0 +1,19 @@
+package com.androiddevs.mvvmnewsapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.androiddevs.mvvmnewsapp.Article
+
+@Dao
+
+interface ArticlleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
