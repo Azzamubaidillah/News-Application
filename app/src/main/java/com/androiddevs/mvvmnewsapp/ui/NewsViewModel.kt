75850474044e7d4159fc4da30300501c3c1f5fe2
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2021-05-21 14:15:04 +0700

    NewsRepo, newsviewmodel, newsprovider

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.mvvmnewsapp.ui
+
+import androidx.lifecycle.ViewModel
+import com.androiddevs.mvvmnewsapp.repository.NewsRepository
+
+class NewsViewModel(
+    val newsRepository: NewsRepository
+) : ViewModel() {
+}
