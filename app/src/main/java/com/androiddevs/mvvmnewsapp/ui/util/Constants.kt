6e1ee3c1dd05f53efe3d2bde249e41e1121228df
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2021-05-21 16:51:55 +0700

    search

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object{
         const val API_KEY = "e78273f6e1a844209893c3e78f7d19db"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
